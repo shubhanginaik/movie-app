319d1214539e358a14a15e04750749f9eb1444e2
shubhanginaik <shubhangi.naik83@gmail.com>
2025-02-22 19:32:22 +0200

    add fetch api functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "./components/Search";
+
+const API_BASE_URL = "https://api.tvmaze.com/shows";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [shows, setShows] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setError("");
+    try {
+      const response = await fetch(`${API_BASE_URL}`);
+      if (!response.ok) {
+        setError("Error while fetching data");
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+      if (data.Response === false) {
+        setError(data.Error || "Something went wrong");
+        setShows([]);
+        return;
+      }
+      console.log(data);
+      setShows(data.slice(0, 20) || []);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
   return (
     <main>
       <div className="pattern" />
@@ -10,12 +43,29 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>
-            Find <span className="text-gradient">Movies</span> You Will Enjoy
-            Without the Hassle
+            Find <span className="text-gradient">Movies/Shows</span> You Will
+            Enjoy Without the Hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
+      {isLoading ? (
+        <p className="text-white">Loading...</p>
+      ) : error ? (
+        <p className="text-red-500">{error}</p>
+      ) : shows.length === 0 ? (
+        <p>No shows found</p>
+      ) : (
+        <ul>
+          {shows.map((show) => (
+            <li key={show.id}>
+              <img src={show.image.medium} alt={show.name} />
+              <h2>{show.name}</h2>
+              <p>{show.summary}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 };
